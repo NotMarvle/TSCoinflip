0c9ea90e565325f89393f5a76b461fd86c2db335
NotMarvle <puffythereall@gmail.com>
2023-02-26 20:08:25 +0100

    ⭐

---
+++ README.md
@@ -0,0 +1,5 @@
+#TSCoinflip
+
+### This Is A Simple TypeScript/JavaScript Website That Uses Math.Random() Function
+
+![image](https://i.gyazo.com/e0abb036a84c60db86853e706d0a50a7.png)
---
+++ dist/app.js
@@ -0,0 +1,10 @@
+function randomOption() {
+    const options = ["heads", "tails"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
+console.log(randomOption());
+document.getElementById("flip").innerHTML = randomOption();
+function tryAgain() {
+    document.getElementById("flip").innerHTML = randomOption();
+}
